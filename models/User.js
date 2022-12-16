4dd904683ba6bf1c39b43a07cd1d0409d1467465
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-12-16 13:28:47 -0700

    SetUp User.js in models with JS

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is required',
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: 'Email is required',
+            unique: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,            
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+    });
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+}
+);
+
+const User = model('User', userSchema);
+modul.exports = User;
